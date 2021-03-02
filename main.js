50e6be520434c01d82595310e3690a5a7136d361
AldanaBaurer <aldana.baurer@sponsorize.ch>
2021-03-02 00:59:42 -0300

    se agregó fecha

--- main.js
+++ main.js
@@ -10,6 +10,28 @@ function generateTime() {
     let mainTime = `${hours}:${minutes}:${seconds}`;
     
     document.getElementById("time").innerHTML = mainTime;
+
+    let fecha = new Date();
+
+    let diaSemana = fecha.getDay();
+    let dia = fecha.getDate();
+    let mes = fecha.getMonth();
+    let year = fecha.getFullYear();
+
+    let pDiasemana = document.getElementById('dayname');
+    let pDia = document.getElementById('daynum');   
+    let pMes = document.getElementById('month');
+    let pYear = document.getElementById('year');
+
+    let semana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    pDiasemana.textContent = semana[diaSemana];
+    
+    pDia.textContent = dia;
+
+    var meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+    pMes.textContent = meses[mes];
+
+    pYear.textContent = year;
   
 }
 
